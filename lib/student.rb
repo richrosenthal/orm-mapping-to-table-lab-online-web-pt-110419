235031c70601e7ea5ac77cbae1a3427ab9f68f58
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-03 17:17:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,15 @@
 class Student
+  
+  attr_accessor :name, :grade
+  
+  def intiialize (name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end 
+  
+  def self.create_table 
+    
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
